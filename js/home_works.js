c9b1c7ff014a00060a3eaa8f244c6be7c4b48d35
kubanovaay <aijamalkubanova27@gmail.com>
2024-02-10 13:17:22 +0600

    js2

--- js/home_works.js
+++ js/home_works.js
@@ -14,3 +14,18 @@ gmailButton.addEventListener('Check gmail', () => {
         gmailResult.style.color = 'red'
     }
 })
+function moveRight(childBlock, parentWidth, currentPosition) {
+    if (currentPosition < parentWidth) {
+        currentPosition++;
+        childBlock.style.left = currentPosition + 'px';
+        setTimeout(() => moveRight(childBlock, parentWidth, currentPosition), 10);
+    }
+}
+
+const parentBlock = document.querySelector('.parent_block');
+const childBlock = document.querySelector('.child_block');
+
+const parentWidth = parentBlock.offsetWidth;
+let currentPosition = 0;
+
+moveRight(childBlock, parentWidth, currentPosition);
