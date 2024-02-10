e5e0e0973ded8914fede92eb3a65d277556c3120
kubanovaay <aijamalkubanova27@gmail.com>
2024-02-10 13:11:39 +0600

    js

---
+++ js/home_works.js
@@ -0,0 +1,16 @@
+const gmailInput = document.querySelector('#gmail_input')
+const gmailButton = document.querySelector('#gmail_button')
+const gmailResult = document.querySelector('#gmail_result')
+
+const regExp = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+
+
+gmailButton.addEventListener('Check gmail', () => {
+    if (regExp.test(gmailInput.value)) {
+        gmailResult.innerHTML = 'OK'
+        gmailResult.style.color = 'green'
+    } else {
+        gmailResult.innerHTML = 'NOT OK'
+        gmailResult.style.color = 'red'
+    }
+})
