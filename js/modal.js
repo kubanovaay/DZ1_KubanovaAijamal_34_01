c391708deb0cb38a77080de4ad642e8f9fbfc7c2
kubanovaay <aijamalkubanova27@gmail.com>
2024-02-15 15:40:05 +0600

    modal js

--- js/modal.js
+++ js/modal.js
@@ -1,27 +1,42 @@
 // MODAL
-const modal = document.querySelector('.modal')
-const modalTriggerButton = document.querySelector('#btn-get')
-const modalCloseButton = document.querySelector('.modal_close')
+
+const modal = document.querySelector('.modal');
+const modalTriggerButton = document.querySelector('#btn-get');
+const modalCloseButton = document.querySelector('.modal_close');
+
+let modalShown = false;
 
 const openModal = () => {
-    modal.style.display = 'block'
-    document.body.style.overflow = 'hidden'
+    modal.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+    modalShown = true;
 }
 
 const closeModal = () => {
-    modal.style.display = 'none'
-    document.body.style.overflow = ''
+    modal.style.display = 'none';
+    document.body.style.overflow = '';
 }
 
-modalTriggerButton.onclick = () => openModal()
+modalTriggerButton.onclick = () => openModal();
 
-modalCloseButton.onclick = () => closeModal()
-
+modalCloseButton.onclick = () => closeModal();
 
 modal.onclick = (event) => {
     if (event.target === modal){
-        closeModal()
+        closeModal();
     }
-
 }
+
+
+const handleScroll = () => {
+    const isBottom = (window.innerHeight + window.scrollY) >= document.body.offsetHeight;
+
+    if (isBottom && !modalShown) {
+        openModal();
+        window.removeEventListener('scroll', handleScroll);
+    }
+}
+
+window.addEventListener('scroll', handleScroll);
+
 
