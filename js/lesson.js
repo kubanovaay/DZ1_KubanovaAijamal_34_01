bbfe3a2f6247e42a9637d5e6fb6c7527a8a62023
kubanovaay <aijamalkubanova27@gmail.com>
2024-02-15 15:21:43 +0600

    js code

--- js/lesson.js
+++ js/lesson.js
@@ -19,40 +19,6 @@ phoneButton.addEventListener('click', () => {
 
 // TAB SLIDER
 
-// const tabContent = document.querySelectorAll('.tab_content_block')
-// const tabs = document.querySelectorAll('.tab_content_item')
-// const tabsParent = document.querySelector('.tab_content_items')
-//
-// let currentIndex = 0
-// const hideTabContents = () => {
-//     tabContent.forEach((item) => {
-//         item.style.display = 'none'
-//     })
-//     tabs.forEach((item) => {
-//         item.classList.remove('tab_content_item_active')
-//     })
-// }
-//
-// const showTabContents = (index = 0) => {
-//     tabContent[index].style.display = 'block'
-//     tabs[index].classList.add('tab_content_item_active')
-// }
-//
-//
-// hideTabContents()
-// showTabContents()
-//
-// tabsParent.onclick = (event) => {
-//     if (event.target.classList.contains('tab_content_item')) {
-//         tabs.forEach((tabItem, tabIndex) => {
-//             if (event.target === tabItem) {
-//                 hideTabContents()
-//                 showTabContents(tabIndex)
-//             }
-//         })
-//     }
-// }
-
 const tabContent = document.querySelectorAll('.tab_content_block');
 const tabs = document.querySelectorAll('.tab_content_item');
 const tabsParent = document.querySelector('.tab_content_items');
